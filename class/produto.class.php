5c83db30d14a00bd2b4cefc38985b1039bb76138
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-10-14 02:37:05 -0300

    feat: changes in classes

---
+++ class/produto.class.php
@@ -0,0 +1,88 @@
+<?php
+require_utils(
+  array( 
+    'replace_values',
+    'prepare_query'
+  )
+);
+
+
+class Produto {
+
+	private $pdo;
+
+
+	public function __construct( $pdo ) {
+		$this->pdo = $pdo;
+	}
+
+	public function categoriaExiste( $categoria ) {
+
+		$stmt = "
+			SELECT 
+				nome 
+			FROM 
+				Categorias 
+			WHERE 
+				LOWER(nome) = LOWER(:categoria)
+		";
+		$find_array = array( ':categoria' => $categoria );
+
+		$categoria = prepare_query( 
+			$stmt, 
+			$find_array, 
+			$this->pdo 
+		);
+
+		if ( $categoria->rowCount() <= 0 ) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	public function getProdutos( $categoria, $qtde ) {
+
+		$stmt = "";
+		$clausulaDeQtde = "";
+		$produtos = array();
+
+		if ($qtde != 'all') {
+			$clausulaDeQtde = "AND P.produto_id <= :qtde";
+		} 
+
+		$stmt = "
+			SELECT
+				P.nome,
+				P.img,
+				C.nome as categoria,
+				P.unidade,
+				P.preco,
+				P.descricao,
+				P.obs
+
+			FROM
+				Produtos P,
+				Categorias C
+
+			WHERE 
+				P.categoria_id = C.categoria_id AND
+				LOWER( C.nome ) = LOWER( :categoria )
+		";
+
+		$find_array = array( ':categoria' => $categoria );
+
+		$stmt = prepare_query( $stmt, $find_array, $this->pdo );
+
+		if ( $stmt->rowCount() > 0 ) {
+			$produtos = $stmt->fetchAll();
+		}
+
+		return $produtos;
+
+	}
+
+}
+
+?>
