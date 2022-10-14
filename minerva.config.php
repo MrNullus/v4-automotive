f89d7a7d78694314855fd49eb0efd55eec1ed8e2
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-10-14 02:41:05 -0300

    feat: updates from utils and create new var global for conection to PDO

--- minerva.config.php
+++ minerva.config.php
@@ -1,13 +1,14 @@
 <?php
-session_start();
+
+$_PDO = new PDO("mysql:host=localhost;dbname=v4_automotive", "root", "");
 
 require_once('./utils/require_utils.utils.php');
 
 require_utils(
 	array(
+		'import_class',
 		'url_base',
-		'renderComponente',
-		'carregar_css', 
+		'renderComponent',
 		'assets'
 	)
 );
