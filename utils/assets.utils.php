b734033cc134d00440bb64de26d7ed3aa06b08e6
Mr. Nullus <70487766+MrNullus@users.noreply.github.com>
2022-10-19 15:42:55 -0300

    changes in logic design

--- utils/assets.utils.php
+++ utils/assets.utils.php
@@ -1,37 +1,11 @@
 <?php  
 
 function assets( $asset, $file ) {
-	
-	$caminho_preparado = url_base().'/public/';
 
-	switch( $asset ) {
-		case 'img':
-			$caminho_preparado .= $asset.'/'.$file;
-			break;
-
-		case 'css':
-			$caminho_preparado .= $asset.'/'.$file;
-			break;
-
-		case 'js':
-			$caminho_preparado .= $asset.'/'.$file;
-			break;
-
-		case 'bootstrap/js':
-			$caminho_preparado .= $asset.'/'.$file;
-			break;
-
-		case 'bootstrap/css':
-			$caminho_preparado .= $asset.'/'.$file;
-			break;
-
-		default:
-			echo "Asset não encontrado";
-			break;
-	}
+	$caminho_preparado = url_base(). '/public/'. $asset .'/'. $file;
 
 	return $caminho_preparado;
-
+	
 }
 
 ?>
