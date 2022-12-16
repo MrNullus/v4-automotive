c09d48457b02a2b0e66139f21df3db0ed27e8818
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-15 23:53:55 -0300

    views: add view

--- views/admin/categorias/deletar.php
+++ views/admin/categorias/deletar.php
@@ -1,5 +1,22 @@
 <?php  
-// TODO:
-// - Fazer a página para deletar uma categoria
+require_once 'C:\\xampp\\htdocs\\v4-automotive\\minerva.config.php';
+
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+	go_to('views/admin/categorias/');
+}
+
+$id = intval($_GET['id']);
+
+global $_PDO;
+import_models(
+  array( 'categoria' )
+);	
+
+$categoria = new Categoria($_PDO);
+if ($categoria->deletar([ ':id' => $id ])) {
+	go_to('views/admin/categorias/?op=true');
+} else {
+	go_to('views/admin/categorias/?op=false');
+}
 
 ?>
